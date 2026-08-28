ab78a537053931e80f7928fa4c0dc5baea0d3b52
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-08-28 21:54:31 +0530

    Create MaskedPhoneNumber.java

---
+++ src/main/java/controlflow/assignment_problems/MaskedPhoneNumber.java
@@ -0,0 +1,22 @@
+public class MaskedPhoneNumber {
+    static String maskPhoneNumber(String phone) {
+        if (phone.length() != 10) {
+            return "Invalid phone number";
+        }
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+        }
+        String lastFour = phone.substring(6);
+        StringBuilder result = new StringBuilder("XXXXXX");
+        result.insert(6, "-");
+        result.append(lastFour);
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maskPhoneNumber("9876543210"));
+        System.out.println(maskPhoneNumber("98765"));
+    }
+}

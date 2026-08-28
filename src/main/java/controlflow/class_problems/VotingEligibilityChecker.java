572e25247ffae9950bbae5bba0501ed7af34e92b
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-08-28 21:33:13 +0530

    Update VotingEligibilityChecker.java

--- src/main/java/controlflow/class_problems/VotingEligibilityChecker.java
+++ src/main/java/controlflow/class_problems/VotingEligibilityChecker.java
@@ -1,1 +1,15 @@
+package controlflow.class_problems;
 
+public class VotingEligibilityChecker {
+
+    static void checkVotingEligibility(int age) {
+        if (age >= 18)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        checkVotingEligibility(20);
+    }
+}

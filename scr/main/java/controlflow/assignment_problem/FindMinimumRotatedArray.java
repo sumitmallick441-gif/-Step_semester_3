28806df78e00641a85c4370216be82a9b93cc6b4
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-09-11 22:54:17 +0530

    Create FindMinimumRotatedArray.java

---
+++ scr/main/java/controlflow/assignment_problem/FindMinimumRotatedArray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class FindMinimumRotatedArray {
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Minimum element: " + findMin(nums));
+
+        sc.close();
+    }
+}

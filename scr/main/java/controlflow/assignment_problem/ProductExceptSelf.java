854f08b4d8f987878691a024877fa55373a73d8c
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-09-11 22:52:19 +0530

    Create ProductExceptSelf.java

---
+++ scr/main/java/controlflow/assignment_problem/ProductExceptSelf.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * right;
+            right = right * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println("Output:");
+        System.out.println(Arrays.toString(result));
+
+        sc.close();
+    }
+}

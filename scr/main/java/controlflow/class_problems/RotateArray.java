8fa0d96471da2b234f100e5bb4bb4eeb90e7a5be
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-09-11 22:59:07 +0530

    Add RotateArray class for array rotation functionality

---
+++ scr/main/java/controlflow/class_problems/RotateArray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RotateArray {
+    public static int[] rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] newArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            newArray[(i + k) % n] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.println("Rotated array: " + Arrays.toString(result));
+
+        sc.close();
+    }
+}

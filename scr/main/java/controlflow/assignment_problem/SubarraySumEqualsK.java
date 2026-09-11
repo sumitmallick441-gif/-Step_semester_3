f13081baa26aff17f5ef58668c14630ef22aa843
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-09-11 22:53:56 +0530

    Create SubarraySumEqualsK.java

---
+++ scr/main/java/controlflow/assignment_problem/SubarraySumEqualsK.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+
+            if (map.containsKey(currentSum - k)) {
+                count += map.get(currentSum - k);
+            }
+
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        System.out.println("Number of subarrays: " + subarraySum(nums, k));
+
+        sc.close();
+    }
+}

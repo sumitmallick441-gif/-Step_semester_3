be4747f73ed6c2b4dda63e9389018384d2074ec0
sumitmallick441-gif <sumitmallick441@gmail.com>
2026-09-19 10:50:45 +0530

    Create MessWallet.java

---
+++ scr/main/java/controlflow/class_problems/MessWallet.java
@@ -0,0 +1,42 @@
+class MessWallet {
+    private double balance;
+
+    public MessWallet(double balance) {
+        if (balance < 0) {
+            System.out.println("Invalid opening balance");
+            this.balance = 0;
+        } else {
+            this.balance = balance;
+        }
+    }
+
+    public void topUp(double amount) {
+        if (amount <= 0) {
+            System.out.println("Top-up rejected");
+        } else {
+            balance += amount;
+            System.out.println("Balance after top-up: " + balance);
+        }
+    }
+
+    public void deduct(double amount) {
+        if (amount > balance) {
+            System.out.println("Deduct rejected: insufficient balance");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        MessWallet wallet = new MessWallet(500);
+
+        wallet.topUp(200);
+        wallet.deduct(1000);
+
+        System.out.println("Final balance: " + wallet.getBalance());
+    }
+}
